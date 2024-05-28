0754eada2b6cb3b258f0813e1c756202fa4387f7
Amir <111203623+rizi-mi@users.noreply.github.com>
2024-05-28 18:09:01 +0300

    Add files via upload

---
+++ lab3/Form1.Designer.cs
@@ -0,0 +1,347 @@
+﻿namespace lab3
+{
+    partial class lab3
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TabC1 = new System.Windows.Forms.TabControl();
+            this.Tab1 = new System.Windows.Forms.TabPage();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Tab2 = new System.Windows.Forms.TabPage();
+            this.textBox13 = new System.Windows.Forms.TextBox();
+            this.textBox14 = new System.Windows.Forms.TextBox();
+            this.textBox15 = new System.Windows.Forms.TextBox();
+            this.textBox16 = new System.Windows.Forms.TextBox();
+            this.textBox17 = new System.Windows.Forms.TextBox();
+            this.textBox18 = new System.Windows.Forms.TextBox();
+            this.textBox333 = new System.Windows.Forms.TextBox();
+            this.textBox555 = new System.Windows.Forms.TextBox();
+            this.textBox111 = new System.Windows.Forms.TextBox();
+            this.textBox444 = new System.Windows.Forms.TextBox();
+            this.textBox666 = new System.Windows.Forms.TextBox();
+            this.textBox222 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TabC1.SuspendLayout();
+            this.Tab1.SuspendLayout();
+            this.Tab2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // TabC1
+            // 
+            this.TabC1.Controls.Add(this.Tab1);
+            this.TabC1.Controls.Add(this.Tab2);
+            this.TabC1.Location = new System.Drawing.Point(33, 30);
+            this.TabC1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.TabC1.Name = "TabC1";
+            this.TabC1.SelectedIndex = 0;
+            this.TabC1.Size = new System.Drawing.Size(240, 316);
+            this.TabC1.TabIndex = 0;
+            // 
+            // Tab1
+            // 
+            this.Tab1.BackColor = System.Drawing.Color.Silver;
+            this.Tab1.Controls.Add(this.label2);
+            this.Tab1.Controls.Add(this.textBox4);
+            this.Tab1.Controls.Add(this.textBox5);
+            this.Tab1.Controls.Add(this.textBox6);
+            this.Tab1.Controls.Add(this.textBox3);
+            this.Tab1.Controls.Add(this.textBox2);
+            this.Tab1.Controls.Add(this.textBox1);
+            this.Tab1.Location = new System.Drawing.Point(4, 25);
+            this.Tab1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Tab1.Name = "Tab1";
+            this.Tab1.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Tab1.Size = new System.Drawing.Size(232, 287);
+            this.Tab1.TabIndex = 0;
+            this.Tab1.Text = "1 Часть";
+            this.Tab1.Click += new System.EventHandler(this.Tab1_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(9, 207);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(177, 22);
+            this.textBox4.TabIndex = 5;
+            this.textBox4.Text = "Сумма";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(9, 118);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.ReadOnly = true;
+            this.textBox5.Size = new System.Drawing.Size(177, 22);
+            this.textBox5.TabIndex = 4;
+            this.textBox5.Text = "Второе слагаемое";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(9, 45);
+            this.textBox6.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.ReadOnly = true;
+            this.textBox6.Size = new System.Drawing.Size(177, 22);
+            this.textBox6.TabIndex = 3;
+            this.textBox6.Text = "Первое слагаемое";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(9, 237);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(177, 22);
+            this.textBox3.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(9, 148);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(177, 22);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged_1);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(9, 75);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(177, 22);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
+            // 
+            // Tab2
+            // 
+            this.Tab2.BackColor = System.Drawing.Color.Silver;
+            this.Tab2.Controls.Add(this.label1);
+            this.Tab2.Controls.Add(this.textBox13);
+            this.Tab2.Controls.Add(this.textBox14);
+            this.Tab2.Controls.Add(this.textBox15);
+            this.Tab2.Controls.Add(this.textBox16);
+            this.Tab2.Controls.Add(this.textBox17);
+            this.Tab2.Controls.Add(this.textBox18);
+            this.Tab2.Controls.Add(this.textBox333);
+            this.Tab2.Controls.Add(this.textBox555);
+            this.Tab2.Controls.Add(this.textBox111);
+            this.Tab2.Controls.Add(this.textBox444);
+            this.Tab2.Controls.Add(this.textBox666);
+            this.Tab2.Controls.Add(this.textBox222);
+            this.Tab2.Location = new System.Drawing.Point(4, 25);
+            this.Tab2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Tab2.Name = "Tab2";
+            this.Tab2.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Tab2.Size = new System.Drawing.Size(232, 287);
+            this.Tab2.TabIndex = 1;
+            this.Tab2.Text = "2 Часть";
+            // 
+            // textBox13
+            // 
+            this.textBox13.Location = new System.Drawing.Point(7, 95);
+            this.textBox13.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox13.Name = "textBox13";
+            this.textBox13.ReadOnly = true;
+            this.textBox13.Size = new System.Drawing.Size(25, 22);
+            this.textBox13.TabIndex = 17;
+            this.textBox13.Text = "c";
+            // 
+            // textBox14
+            // 
+            this.textBox14.Location = new System.Drawing.Point(7, 196);
+            this.textBox14.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox14.Name = "textBox14";
+            this.textBox14.ReadOnly = true;
+            this.textBox14.Size = new System.Drawing.Size(25, 22);
+            this.textBox14.TabIndex = 16;
+            this.textBox14.Text = "e";
+            // 
+            // textBox15
+            // 
+            this.textBox15.Location = new System.Drawing.Point(7, 31);
+            this.textBox15.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox15.Name = "textBox15";
+            this.textBox15.ReadOnly = true;
+            this.textBox15.Size = new System.Drawing.Size(25, 22);
+            this.textBox15.TabIndex = 15;
+            this.textBox15.Text = "a";
+            // 
+            // textBox16
+            // 
+            this.textBox16.Location = new System.Drawing.Point(7, 164);
+            this.textBox16.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox16.Name = "textBox16";
+            this.textBox16.ReadOnly = true;
+            this.textBox16.Size = new System.Drawing.Size(25, 22);
+            this.textBox16.TabIndex = 14;
+            this.textBox16.Text = "d";
+            // 
+            // textBox17
+            // 
+            this.textBox17.Location = new System.Drawing.Point(7, 228);
+            this.textBox17.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox17.Name = "textBox17";
+            this.textBox17.ReadOnly = true;
+            this.textBox17.Size = new System.Drawing.Size(25, 22);
+            this.textBox17.TabIndex = 13;
+            this.textBox17.Text = "f";
+            // 
+            // textBox18
+            // 
+            this.textBox18.Location = new System.Drawing.Point(7, 63);
+            this.textBox18.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox18.Name = "textBox18";
+            this.textBox18.ReadOnly = true;
+            this.textBox18.Size = new System.Drawing.Size(25, 22);
+            this.textBox18.TabIndex = 12;
+            this.textBox18.Text = "b";
+            // 
+            // textBox333
+            // 
+            this.textBox333.Location = new System.Drawing.Point(41, 95);
+            this.textBox333.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox333.Name = "textBox333";
+            this.textBox333.Size = new System.Drawing.Size(139, 22);
+            this.textBox333.TabIndex = 11;
+            this.textBox333.TextChanged += new System.EventHandler(this.textBox333_TextChanged);
+            // 
+            // textBox555
+            // 
+            this.textBox555.Location = new System.Drawing.Point(41, 196);
+            this.textBox555.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox555.Name = "textBox555";
+            this.textBox555.Size = new System.Drawing.Size(139, 22);
+            this.textBox555.TabIndex = 10;
+            // 
+            // textBox111
+            // 
+            this.textBox111.Location = new System.Drawing.Point(41, 31);
+            this.textBox111.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox111.Name = "textBox111";
+            this.textBox111.Size = new System.Drawing.Size(139, 22);
+            this.textBox111.TabIndex = 9;
+            this.textBox111.TextChanged += new System.EventHandler(this.textBox111_TextChanged);
+            // 
+            // textBox444
+            // 
+            this.textBox444.Location = new System.Drawing.Point(41, 164);
+            this.textBox444.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox444.Name = "textBox444";
+            this.textBox444.Size = new System.Drawing.Size(139, 22);
+            this.textBox444.TabIndex = 8;
+            this.textBox444.TextChanged += new System.EventHandler(this.textBox444_TextChanged);
+            // 
+            // textBox666
+            // 
+            this.textBox666.Location = new System.Drawing.Point(41, 228);
+            this.textBox666.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox666.Name = "textBox666";
+            this.textBox666.Size = new System.Drawing.Size(139, 22);
+            this.textBox666.TabIndex = 7;
+            // 
+            // textBox222
+            // 
+            this.textBox222.Location = new System.Drawing.Point(41, 63);
+            this.textBox222.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox222.Name = "textBox222";
+            this.textBox222.Size = new System.Drawing.Size(139, 22);
+            this.textBox222.TabIndex = 6;
+            this.textBox222.TextChanged += new System.EventHandler(this.textBox222_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(75, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 16);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "Расчеты:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(75, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 16);
+            this.label2.TabIndex = 19;
+            this.label2.Text = "Расчеты:";
+            // 
+            // lab3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(309, 396);
+            this.Controls.Add(this.TabC1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "lab3";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.Text = "lab3";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.TabC1.ResumeLayout(false);
+            this.Tab1.ResumeLayout(false);
+            this.Tab1.PerformLayout();
+            this.Tab2.ResumeLayout(false);
+            this.Tab2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl TabC1;
+        private System.Windows.Forms.TabPage Tab1;
+        private System.Windows.Forms.TabPage Tab2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox13;
+        private System.Windows.Forms.TextBox textBox14;
+        private System.Windows.Forms.TextBox textBox15;
+        private System.Windows.Forms.TextBox textBox16;
+        private System.Windows.Forms.TextBox textBox17;
+        private System.Windows.Forms.TextBox textBox18;
+        private System.Windows.Forms.TextBox textBox333;
+        private System.Windows.Forms.TextBox textBox555;
+        private System.Windows.Forms.TextBox textBox111;
+        private System.Windows.Forms.TextBox textBox444;
+        private System.Windows.Forms.TextBox textBox666;
+        private System.Windows.Forms.TextBox textBox222;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ lab3/Form1.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab3
+{
+    public partial class lab3 : Form
+    {
+        public lab3()
+        {
+            InitializeComponent();
+        }
+
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        string s;
+        double a, b, c, d, e1, f1 = 0;
+
+        private void Tab1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox444_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+            if (textBox1.Text == "")
+            {
+                a = 0;
+            }
+            else
+            {
+                s = textBox1.Text;
+                a = Convert.ToDouble(s);
+            }
+            textBox3.Text = "Идет расчет";
+        }
+
+        private void textBox2_TextChanged_1(object sender, EventArgs e)
+        {
+            if (textBox2.Text == "")
+            {
+                b = 0;
+            }
+
+            else
+            {
+                s = textBox2.Text;
+                b = Convert.ToDouble(s);
+            }
+
+            c = a + b;
+
+            textBox3.Text = Convert.ToString(c);
+
+        }
+
+
+        private void textBox111_TextChanged(object sender, EventArgs e)
+        {
+            
+            if (textBox111.Text == "")
+            {
+                a = 0;
+            }
+            else
+            {
+
+                a = Convert.ToDouble(textBox111.Text);
+
+            }
+            d = a + b;
+            e1 = d * c;
+            f1 = e1 / 2;
+
+            if (d == 0)
+            {
+                textBox444.Text = "Идет расчет";
+            }
+            if (e1 == 0){
+
+                textBox555.Text = "Идет расчет";
+            }
+            if (f1 == 0)
+            {
+                textBox666.Text = "Идет расчет";
+            }
+            else
+            {
+                
+                textBox444.Text = d.ToString();
+                textBox555.Text = e1.ToString();
+                textBox666.Text = f1.ToString();
+            }
+        }
+
+        private void textBox222_TextChanged(object sender, EventArgs e)
+        {
+            
+           
+            if (textBox222.Text == "")
+            {
+                b = 0;
+            }
+            else
+            {
+
+                b = Convert.ToDouble(textBox222.Text);
+
+            }
+
+
+            d = a + b;
+            e1 = d * c;
+            f1 = e1 / 2;
+            if (d == 0)
+            {
+                textBox444.Text = "Идет расчет";
+            }
+            if (e1 == 0)
+            {
+                textBox555.Text = "Идет расчет";
+            }
+            if (f1 == 0)
+            {
+                textBox666.Text = "Идет расчет";
+            }
+            else
+            {
+                
+                textBox444.Text = d.ToString();
+                textBox555.Text = e1.ToString();
+                textBox666.Text = f1.ToString();
+            }
+        }
+
+        
+        private void textBox333_TextChanged(object sender, EventArgs e)
+        {
+            
+            
+            if (textBox333.Text == "")
+            {
+                c = 0;
+            }
+            else
+            {
+
+                c = Convert.ToDouble(textBox333.Text);
+
+            }
+
+
+            
+            d = a + b;
+            e1 = d * c;
+            f1 = e1 / 2;
+
+            
+            if (d == 0)
+            {
+                textBox444.Text = "Идет расчет";
+            }
+            if (e1 == 0)
+            {
+                textBox555.Text = "Идет расчет";
+            }
+            if (f1 == 0)
+            {
+                textBox666.Text = "Идет расчет";
+            }
+            else
+            {
+                
+                textBox444.Text = d.ToString();
+                textBox555.Text = e1.ToString();
+                textBox666.Text = f1.ToString();
+            }
+        }
+    }
+
+}
+
